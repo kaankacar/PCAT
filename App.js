adc4d222e084dd0774a523ec976f2f8b1f3238db
kaankacar <kaankacar02@gmail.com>
2022-12-26 00:11:14 +0300

    Installed express and nodemon. Wrote sample code

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log("it's working");
-};
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        name: "Sample",
+        desc: "test desc"
+    }
+    res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`App started ad ${port}th port`);
+});
