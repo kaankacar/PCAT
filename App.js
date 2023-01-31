d709ac3406f0fe7d3041c98e293acc9fab2151f9
kaankacar <kaankacar02@gmail.com>
2023-01-31 23:28:11 +0300

    practiced with middleware and defined static files

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
 
 const port = 3000;
 
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
+
+app.use(express.static('public'));
+app.use(myLogger);
+
 app.get('/', (req, res) => {
-    const photo = {
-        id: 1,
-        name: "Sample",
-        desc: "test desc"
-    }
-    res.send(photo);
+  res.sendFile(path.resolve(__dirname,"temp/index.html"))
 });
 
 app.listen(port, () => {
